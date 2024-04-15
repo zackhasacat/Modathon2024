8ff975c1f3160e7c20ba769ba5f2d33c834ac851
Zackhasacat <tobias@tribble.dev>
2024-04-15 12:49:46 -0500

    Update Apocolypse mode

--- Vault/scripts/MorroVault/apoc.lua
+++ Vault/scripts/MorroVault/apoc.lua
@@ -10,12 +10,37 @@ local anim = require('openmw.animation')
 local function startsWith(inputString, startString)
     return string.sub(inputString, 1, string.len(startString)) == startString
 end
-
+local itemBlacklist = {"a_siltstrider"}
+math.randomseed(os.time())
 local function isInVault(obj)
    return startsWith(obj.cell.name, "Resdaynia Sanctuary")
 end
-
+local processedActors = {}
 local function onActorActive(act)
+    if  processedActors[act.id] or isInVault(act) then
+        return
+    end
+    local randomNumber = math.random(0, 100)
+    processedActors[act.id] = true
+    if act.type == types.NPC then
+
+        local race = types.NPC.record(act).race
+        if race:lower() == "dark elf"and randomNumber < 30  then
+            for index, value in ipairs(types.Actor.inventory(act):getAll()) do
+                value:remove()
+            end
+            local club = world.createObject("iron club")
+            club:moveInto(act)
+            act:sendEvent("makeAgressive")
+            return
+        elseif randomNumber < 40 then
+            local newCreature = world.createObject("ash_ghoul")
+            newCreature:teleport(act.cell,act.position)
+        elseif randomNumber < 30 then
+            local newObj = world.createObject("Sound_Haunted00")
+            newObj:teleport(act.cell,act.position)
+        end
+    end
     if not isInVault(act) then
         act:remove()
     end
@@ -25,7 +50,14 @@ local function onObjectActive(act)
         if not isInVault(act) then
             act:remove()
             act.enabled = false
-            print("Kill light")
+            return
+        end
+    end
+    for index, value in ipairs(itemBlacklist) do
+        if value == act.recordId then
+            
+            act:remove()
+            act.enabled = false
         end
     end
 end
